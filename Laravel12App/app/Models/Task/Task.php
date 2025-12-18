16a20837c817b24c4e66d5a85ba43251f7ed4e58
vincent <56928557+vito-hub@users.noreply.github.com>
2025-12-18 12:40:08 -0600

    Task update

--- Laravel12App/app/Models/Task/Task.php
+++ Laravel12App/app/Models/Task/Task.php
@@ -2,9 +2,27 @@
 
 namespace App\Models\Task;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
 
 class Task extends Model
 {
-    //
+    use HasFactory, Notifiable;
+    protected $table = 'task_tables';
+    protected $fillable = [
+      'name',
+      'completed',
+      'user_id',
+      'created_by'
+    ];
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(User::class,'user_id');
+    }
+
+
 }
